70587bb9fb016b84ffff26a0ed9f278dd16ef948
Panzerhandschuh <TylerBeff@gmail.com>
2018-10-20 12:55:27 -0400

    Added unit tests

---
+++ Assets/Scripts/Tests/DdsFileTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System.IO;
+
+namespace UnityDds.Tests
+{
+	public class DdsFileTests
+	{
+		[Test]
+		public void CanReadDdsFile()
+		{
+			const string path = @"Assets\Scenes\Texture.DDS";
+			using (var stream = File.Open(path, FileMode.Open))
+			using (var reader = new BinaryReader(stream))
+			{
+				var file = DdsFile.Deserialize(reader);
+				Assert.NotNull(file);
+			}
+		}
+	}
+}
